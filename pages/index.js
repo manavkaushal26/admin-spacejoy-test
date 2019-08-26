af2f650f0053e0d23eb11d15d33b7d2a23310281
Saurabh Sachan <saurabhdsachan@gmail.com>
2019-08-26 15:14:26 +0530

    css grid added

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,58 @@ function index() {
 			</ul>
 			<h1>THIS IS OUR HOMEPAGE.</h1>
 			<p>Text</p>
+			<div className="row">
+				<div className="col-xs-7">
+					<div className="box box-container">
+						<div className="row">
+							<div className="col-xs-9">
+								<div className="box-first box-container">
+									<div className="row">
+										<div className="col-xs-4">
+											<div className="box-nested" />
+										</div>
+										<div className="col-xs-8">
+											<div className="box-nested" />
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-xs-3">
+								<div className="box-first box-container">
+									<div className="row">
+										<div className="col-xs">
+											<div className="box-nested" />
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-xs-5">
+					<div className="box box-container">
+						<div className="row">
+							<div className="col-xs-12">
+								<div className="box-first box-container">
+									<div className="row">
+										<div className="col-xs-6">
+											<div className="box-nested" />
+										</div>
+										<div className="col-xs-6">
+											<div className="box-nested" />
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-xs-12">
+					<div className="box">Hi</div>
+				</div>
+			</div>
 		</Layout>
 	);
 }
