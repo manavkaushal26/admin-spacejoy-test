1a935afb7a4c97ec543a086c7fd59b5fe7571120
Saurabh Sachan <saurabhdsachan@gmail.com>
2019-08-30 21:30:26 +0530

    mock fix

--- _utils/faqMock.js
+++ _utils/faqMock.js
@@ -1,195 +1,164 @@
 const data = [
 	{
 		tag: "Account & Payment",
-		imgUrl: `${this.imgUrl}FAQ/What types of online payment are accepted.jpg`,
 		question: "What payment types do you accept?",
 		answer: `We accept all standard forms of payment such as Visa, Mastercard & American Express. You can use any of this to pay for your package and for the products featured in your final design.`
 	},
 	{
 		tag: "Account & Payment",
-		imgUrl: `${this.imgUrl}FAQ/What happens if I forget my login and password.jpg`,
 		question: "What happens if I forget my login and/or password?",
 		answer: `Click on the forgot username / password button and enter your registered email id. Our support team will generate your username/password right away.`
 	},
 	{
 		tag: "Account & Payment",
-		imgUrl: `${this.imgUrl}FAQ/What if I want to cancel my payment and initiate a refund.jpg`,
 		question: "What if I want to cancel my payment and initiate a refund?",
 		answer: `We want you to love your designs and your new space. If at any point you are dissatisfied, let us know at <a href="mailto:hello@spacejoy.com?subject=&body=">hello@spacejoy.com</a> and we will work with you to ensure a seamless experience .`
 	},
 	{
 		tag: "Customer Support",
-		imgUrl: `${this.imgUrl}FAQ/Who are your designers.jpg`,
 		question: "What is the best way to contact Spacejoy?",
 		answer: `Write to us at <a href="mailto:hello@spacejoy.com?subject=&body=">hello@spacejoy.com</a> or schedule a call.`
 	},
 	{
 		tag: "Get Started",
-		imgUrl: `${this.imgUrl}FAQ/How do I get Started.jpg`,
 		question: "How do I get started?",
 		answer: `Login to our platform and sign up for one of the <a href="https://spacejoy.com/pricing">packages</a>, depending on your needs. Our designers will get in touch with you before they process your request to get all the necessary details.`
 	},
 	{
 		tag: "Get Started",
-		imgUrl: `${this.imgUrl}FAQ/Free trial period.jpg`,
 		question: "Is there a free trial period?",
 		answer: `No trial period is offered. However there are some features on the app that can be experienced for free. Such as, take up a design challenge, find out your home design style or design a standard room to get the hang of  what our interactive 3D app  can offer.`
 	},
 	{
 		tag: "Get Started",
-		imgUrl: `${this.imgUrl}FAQ/Where can i dind designs for inspiration from spacejoy designers.jpg`,
 		question: "Where can I see some ideas from your designers for inspiration?",
 		answer: `Login to our platform and go through an array of samples that have been created by our designers. Some of these include completed projects that Spacejoy has successfully transformed. We are sure you’ll find inspiration that will  get your creative juices flowing.`
 	},
 	{
 		tag: "Get Started",
-		imgUrl: `${this.imgUrl}FAQ/canImeethedesignerfacetoface.jpg`,
 		question: "Once I get started, can I meet the design team face to face?",
 		answer: `We have designers from around the country. If meeting the designer face to face is important to you then we will try to match you with someone around your location. While signing up, be sure to fill in your location and phone number so we can help.`
 	},
 	{
 		tag: "Get Started",
-		imgUrl: `${this.imgUrl}FAQ/Where do you source products  from.jpg`,
 		question: "What brands do you source the products from?",
 		answer: `The products featured in your designs are from various leading brands in the country. Currently, all the products are locally sourced and we don’t have any retailers outside America`
 	},
 	{
 		tag: "Get Started",
-		imgUrl: `${this.imgUrl}FAQ/What happens once I purchase products featured in my designs.jpg`,
 		question: "What happens once I purchase products featured in my designs?",
 		answer: `Once you have finalised the design, our customer satisfaction specialist will give you a curated shopping list of products from various brands and the date of delivery for each product, as it is retailer specific. Once we receive the sign off from you, the order is placed and all you have to do is sit back and wait for it to be delivered to your doorstep. `
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/What is space joy.jpeg`,
 		question: "What is Spacejoy?",
 		answer:
 			"Spacejoy is an online interior design service that helps with designing your actual space in 3D. You can either design it yourself, ask for an expert to design it for you or even collaborate with the design team. The final design features products that can be bought right away from furniture and home decor stores, across America."
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/photo-1556022363-5187bfa13ad9.jpg`,
 		question: "Why Spacejoy?",
 		answer:
 			"Spacejoy’s interactive 3D app is one of the easiest ways to design your home. We provide full access to some of the best interior designers from around the country. You can see your actual room in 3D complete with furniture and decor that will help you make an informed purchase decision."
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/Who are your designers.jpg`,
 		question: "Who are your designers?",
 		answer:
 			"Our interior designers are all professionals from across the United States. Designers who apply to Spacejoy with their credentials are reviewed and the ones with potential are asked to make several designs based on actual user requests. The designs are then reviewed by our head of design before on-boarding."
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/canIbecomeaspacejoydesigner.jpg`,
 		question: "I am interested in design, can I become a Spacejoy designer?",
 		answer:
 			"If you are passionate about designing, log on to our app and design various rooms of your choice and submit them. Make sure to submit different styles of design. If your designs meet our standards, then we will coach you before putting you on to actual projects."
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/What is the design fee.jpg`,
 		question: "What is the design fee?",
 		answer: `We have three packages - <a href="https://spacejoy.com/pricing">Consult</a>, <a href="https://spacejoy.com/pricing">Classic</a> and <a href="https://spacejoy.com/pricing">Premium</a>. You'll find details of the packages in our <a href="https://spacejoy.com/pricing">Pricing Section</a>. Pick the one that best suits your requirements.`
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/Spacejoy is a one stop solution.jpg`,
 		question: "How is Spacejoy a one-stop design solution?",
 		answer:
 			"You can design your room yourself or get one of our expert designers to help you design the space. Once designed the products that are featured in the design can be shopped directly from Spacejoy. Our stylist will manage your orders for you. "
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/Spacejpy work within my budget.jpg`,
 		question: "Can Spacejoy work within my budget? ",
 		answer:
 			"Yes, we can work under your budget. This will not alter the quality of designs or the suggestions that are made. We have super curators who will scout all stores across the internet to ensure your space gets the best makeover within your budget."
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/How long does it take for a project to complete from start to finish.jpg`,
 		question: "How long does it take for a design to be complete, from start to finish?",
 		answer: `If you choose the ‘<a href="https://spacejoy.com/pricing">Classic</a>’ or the ‘<a href="https://spacejoy.com/pricing">Consult</a>’ package, your initial 2 designs will be delivered within 7 days. If you choose the ‘<a href="https://spacejoy.com/pricing">Premium</a>’ package, your initial 2 designs will be delivered within 5 days. Once the initial designs are delivered, assuming there will be revisions, the final design post your feedback and revisions will be delivered along with the shopping list within 5 days from the date of the initial design delivery.`
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/What will happen if I am unhappy with my design.jpg`,
 		question: "What do I do if I am unhappy with a design?",
 		answer:
 			"Spacejoy designers will spend time understanding your needs and criteria before they start designing. Based on your inputs, our designers will share 2 initial designs with you. In case you don’t like them or need revisions, our designers will work with you till you land on a design that you love.  If you are still not convinced with the output, we will refund your money."
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/How frequently can I reach out to my designer assisting me.jpg`,
 		question: "How frequently can I reach out to the designers/s or the design team assisting me?",
 		answer:
 			"You have unlimited access to our designers during working hours. Make an appointment in advance and feel free to reach out to them."
 	},
 	{
 		tag: "Who are we?",
-		imgUrl: `${this.imgUrl}FAQ/How long does it take for a project to complete from start to finish.jpg`,
 		question: "Where can I find the Spacejoy app?",
 		answer: "Spacejoy is available on both android and IOS stores.  <Link to the android store and the IOS store>"
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/What kind of spaces will spacejoy design.jpg`,
 		question: "What kind of spaces can Spacejoy design for me?",
 		answer: `We can design your living room, bedroom, dining area, sunroom and entryway. If you however have any special requests we’ll be happy to discuss. Write to us at <a href="mailto:hello@spacejoy.com?subject=&body=">hello@spacejoy.com</a>`
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/Different packages.jpg`,
 		question: "Are there different design packages?",
 		answer: `Yes, there are three types of design packages. Refer to it in detail in our <a href="https://spacejoy.com/packages">Pricing Section</a> `
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/How can I get in touch with an expert designer.jpg`,
 		question: "How can I get in touch with Spacejoy’s expert design team?",
 		answer: `Sign up for either the <a href="https://spacejoy.com/pricing">Classic</a> or <a href="https://spacejoy.com/pricing">Premium</a> package which will give you access to our designers. They can help you with all your design related queries and guide you through the entire process.`
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/Can spacejoy design my kitchen or bathroom.jpg`,
 		question: "Can Spacejoy design my kitchen or bathroom?",
 		answer: `We currently don’t extend our services to bathrooms and kitchens that may require remodeling. However, if you need help with add-on or plug-n-play solutions, we can help. Write to us at <a href="mailto:hello@spacejoy.com?subject=&body=">hello@spacejoy.com</a> with your requirements and we’ll be happy to work something out.`
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/Spacejoy is a one stop solution.jpg`,
 		question: "Will Spacejoy help me in sourcing all the products/items/furniture for each room?",
 		answer: `Spacejoy will give you an exhaustive shopping list of all the products featured in your final design from various stores. If you need assistance in placing and managing the order, we can assign a stylist to help you.`
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/Can spacejoy help with remodelling and redesigning.jpg`,
 		question: "Can Spacejoy help with renovation and remodelling projects?",
 		answer: `No, Spacejoy currently does not take up renovation or remodeling projects. At the moment we only offer plug-n-play services.`
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/Can spacejoy design a commercial space.jpg`,
 		question: "Can Spacejoy design a commercial space?",
 		answer: `Yes, we can take up commercial space designing regardless of the size (big and small). You can get in touch with us at <a href="mailto:hello@spacejoy.com?subject=&body=">hello@spacejoy.com</a> and we’ll assign one of our designers to help you with your project. `
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/How long does it take for a project to complete from start to finish.jpg`,
 		question: "How long does it take for a design to be complete, from start to finish?",
 		answer: `If you choose the ‘<a href="https://spacejoy.com/pricing">Classic</a>’ or the ‘<a href="https://spacejoy.com/pricing">Consult</a>’ package, your 2 initial designs will be delivered within 7 days. If you choose the ‘<a href="https://spacejoy.com/pricing">Premium</a>’ package, your 2 initial designs will be delivered within 5 days. These designs will go through revisions based on your feedback. The final design, after incorporating your feedback, will be delivered to you within 5 days from the date of delivery of the initial design. This will include a shopping list of the products featured in your final design.`
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/What will happen if I am unhappy with my design.jpg`,
 		question: "What do I do if I am unhappy with a design?",
 		answer: `Spacejoy designers will spend time understanding your needs and criteria before they start your project. Based on your inputs, our designers will share 2 initial designs with you. In case you’re unhappy or need revisions, our designers will work with you till you land on a design that you love.  If you are still not convinced with the output, we will refund your money. `
 	},
 	{
 		tag: "Features & Services",
-		imgUrl: `${this.imgUrl}FAQ/How frequently can I reach out to my designer assisting me.jpg`,
 		question: "How frequently can I reach out to the designers/s or the design team assisting me?",
 		answer: `You have unlimited access to our designers during working hours. Make an appointment in advance and feel free to reach out to them. `
 	}
