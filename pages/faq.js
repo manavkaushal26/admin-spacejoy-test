6f3441f45149dfa6e30e93fd77bb61c17df2e90b
JoyBook1 <joybook1@JoyBook1s-MacBook-Pro.local>
2019-09-15 15:45:59 +0530

    Faq update

--- pages/faq.js
+++ pages/faq.js
@@ -1,4 +1,3 @@
-import Divider from "@components/Divider";
 import Image from "@components/Image";
 import FAQCard from "@sections/Cards/faq";
 import Layout from "@sections/Layout";
@@ -6,14 +5,15 @@ import { company } from "@utils/config";
 import FaqData from "@utils/faqMock";
 import IndexPageMeta from "@utils/meta";
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { PureComponent } from "react";
 import styled from "styled-components";
 
 const TopicStyled = styled.div`
 	display: flex;
-	border: 1px solid ${({ theme }) => theme.colors.bg.dark1};
+	border: 1px solid ${({ theme, active }) => (active ? theme.colors.primary : theme.colors.bg.dark1)};
+	color: ${({ theme, active }) => (active ? theme.colors.primary : theme.colors.fc.dark1)};
 	padding: 1rem;
-	border-radius: 2px;
+	border-radius: 5px;
 	transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
 	&:hover {
 		box-shadow: 0 0 10px 0px rgba(0, 0, 0, 0.1);
@@ -25,89 +25,79 @@ const TopicStyled = styled.div`
 		&:nth-child(2) {
 			flex: 2;
 		}
-		&:last-child {
-			flex: 1;
-			text-align: right;
-		}
 	}
 `;
 
-function faq() {
-	const [openId, setOpenId] = useState(0);
-	const handleOpenId = id => setOpenId(id);
-	return (
-		<Layout header="solid">
-			<Head>
-				{IndexPageMeta}
-				<title>Pricing | {company.product}</title>
-			</Head>
-			<div className="container">
-				<Image size="354px" src="https://res.cloudinary.com/spacejoy/image/upload/v1567248692/web/faq_dgczvi.jpg" />
-				<div className="grid">
-					<div className="col-xs-12">
-						<h1>Frequently Asked Questions</h1>
-						<h3>Topics</h3>
-						<div className="grid">
-							<div className="col-xs-12 col-md-3 ">
-								<TopicStyled>
-									<Image size="xs" src="https://image.flaticon.com/icons/svg/907/907830.svg" />
-									<span>Who are we?</span>
-									<span>92</span>
-								</TopicStyled>
+class faq extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			openId: 0,
+			filteredData: FaqData,
+			activeTag: "",
+			filteredTag: ["Who are we?", "Account & Payment", "Customer Support", "Get Started", "Features & Services"]
+		};
+	}
+
+	handleOpenId = openId => this.setState({ openId });
+
+	handleFilter = filter => {
+		const filteredData = FaqData.filter(item => item.tag === filter);
+		this.setState({ filteredData, activeTag: filter });
+	};
+
+	render() {
+		const { openId, filteredData, filteredTag, activeTag } = this.state;
+		return (
+			<Layout header="solid">
+				<Head>
+					{IndexPageMeta}
+					<title>Pricing | {company.product}</title>
+				</Head>
+				<div className="container">
+					<Image size="354px" src="https://res.cloudinary.com/spacejoy/image/upload/v1567248692/web/faq_dgczvi.jpg" />
+					<div className="grid">
+						<div className="col-xs-12">
+							<h1>Frequently Asked Questions</h1>
+							<h3>Topics</h3>
+							<div className="grid">
+								{filteredTag &&
+									filteredTag.map(item => (
+										<div className="col-xs-12 col-md-3" key={Math.random() * 10}>
+											<TopicStyled active={item === activeTag} onClick={() => this.handleFilter(item)}>
+												{item === activeTag && (
+													<Image
+														size="xs"
+														src="https://res.cloudinary.com/spacejoy/image/upload/v1568542112/shared/tick-circle_fgeebs.svg"
+													/>
+												)}
+												<span>{item}</span>
+											</TopicStyled>
+										</div>
+									))}
 							</div>
-							<div className="col-xs-12 col-md-3 ">
-								<TopicStyled>
-									<Image size="xs" src="https://image.flaticon.com/icons/svg/907/907830.svg" />
-									<span>Features & Services</span>
-									<span>92</span>
-								</TopicStyled>
-							</div>
-							<div className="col-xs-12 col-md-3 ">
-								<TopicStyled>
-									<Image size="xs" src="https://image.flaticon.com/icons/svg/907/907830.svg" />
-									<span>Get Started</span>
-									<span>92</span>
-								</TopicStyled>
-							</div>
-							<div className="col-xs-12 col-md-3 ">
-								<TopicStyled>
-									<Image size="xs" src="https://image.flaticon.com/icons/svg/907/907830.svg" />
-									<span>Account & Payment</span>
-									<span>92</span>
-								</TopicStyled>
-							</div>
-							<div className="col-xs-12 col-md-3 ">
-								<TopicStyled>
-									<Image size="xs" src="https://image.flaticon.com/icons/svg/907/907830.svg" />
-									<span>Account & Payment</span>
-									<span>92</span>
-								</TopicStyled>
-							</div>
-							<div className="col-xs-12 col-md-3 ">
-								<TopicStyled>
-									<Image size="xs" src="https://image.flaticon.com/icons/svg/907/907830.svg" />
-									<span>Customer Support</span>
-									<span>92</span>
-								</TopicStyled>
-							</div>
-						</div>
-						<Divider />
-						<div className="grid">
-							<div className="col-xs-12 col-xs-6">
-								<h3>All FAQ&apos;s</h3>
-								{FaqData.map((item, index) => (
-									<FAQCard key={Math.random() * 10} open={index === openId} handleOpenId={handleOpenId} quesId={index}>
-										<FAQCard.Question>{item.question}</FAQCard.Question>
-										<FAQCard.Answer>{item.answer}</FAQCard.Answer>
-									</FAQCard>
-								))}
+							<div className="grid">
+								<div className="col-xs-12 col-xs-6">
+									<h3>FAQ&apos;s {activeTag ? ` - ${activeTag}` : `All`}</h3>
+									{filteredData.map((item, index) => (
+										<FAQCard
+											key={Math.random() * 10}
+											open={index === openId}
+											handleOpenId={this.handleOpenId}
+											quesId={index}
+										>
+											<FAQCard.Question>{item.question}</FAQCard.Question>
+											<FAQCard.Answer>{item.answer}</FAQCard.Answer>
+										</FAQCard>
+									))}
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</Layout>
-	);
+			</Layout>
+		);
+	}
 }
 
 export default faq;
