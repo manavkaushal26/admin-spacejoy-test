8ca354ef1e6f0aa61d93ecf115d196f2860e0135
Sagar Dixit <sagardixit92@github.com>
2020-12-11 18:14:46 +0530

    api endpoint changes

--- _sections/Dashboard/userProjectMainPanel/ProjectDesignInteractionPanel/StyleQuizResponse/index.tsx
+++ _sections/Dashboard/userProjectMainPanel/ProjectDesignInteractionPanel/StyleQuizResponse/index.tsx
@@ -1,6 +1,5 @@
 import Image from "@components/Image";
 import fetcher from "@utils/fetcher";
-import { Card } from "antd";
 import React, { useEffect, useState } from "react";
 
 interface LogObject {
@@ -17,7 +16,7 @@ const UserStyleQuizResponse: React.FC<{ userId: string }> = ({ userId }) => {
 	const [userStyleLogs, setUserStyleLogs] = useState([]);
 
 	const fetchUserStyleResponse = async (userId: string): Promise<string> => {
-		const endPoint = `/quiz/v1/result/store/${userId}`;
+		const endPoint = `/v1/quizUsers/${userId}/results`;
 		let err;
 		try {
 			const res = await fetcher({ endPoint, method: "GET" });
@@ -36,7 +35,7 @@ const UserStyleQuizResponse: React.FC<{ userId: string }> = ({ userId }) => {
 	};
 
 	const fetchUserQuizLog = async (quizId: string): Promise<Array<LogShape>> => {
-		const endPoint = `/quiz/v1/logs/${quizId}`;
+		const endPoint = `/v1/quizzes/${quizId}/logs`;
 		let err;
 		try {
 			const res = await fetcher({ endPoint, method: "GET" });
@@ -53,35 +52,37 @@ const UserStyleQuizResponse: React.FC<{ userId: string }> = ({ userId }) => {
 	const fetchResults = async (userId: string): Promise<void> => {
 		const userStyleQuizResponse = await fetchUserStyleResponse(userId);
 		if (userStyleQuizResponse) {
-			console.log(userStyleQuizResponse);
 			const quizResultLog = await fetchUserQuizLog(userStyleQuizResponse);
 			setUserStyleLogs(quizResultLog);
 		}
 	};
 	useEffect(() => {
-		console.log("user id", userId);
 		if (userId) {
 			fetchResults(userId);
 		}
 	}, [userId]);
-
 	return (
 		<div>
 			{userStyleLogs.map(userStyle => {
 				const styleCategory = Object.keys(userStyle)[0];
 				const values = Object.values(userStyle);
 				return (
-					<div key={userStyle.id}>
-						<h2>{styleCategory}</h2>
-						{values.map(
-							(value: LogObject): JSX.Element => (
-								<Card
-									key={userStyle.id}
-									hoverable
-									cover={<Image src={`q_70,w_300,h_180/${value?.cdn}`} width='100%' />}
-								></Card>
-							)
-						)}
+					<div key={userStyle?.id}>
+						<h2 style={{ textTransform: "capitalize" }}>{styleCategory}</h2>
+						{values[0] &&
+							values[0].map(
+								(value: LogObject): JSX.Element => {
+									console.log("value", value);
+									return (
+										<>
+											<Image style={{ maxWidth: 300 }} src={`q_70,w_300,h_180/${value?.cdn}`} width='100%' />
+											&nbsp;
+										</>
+									);
+								}
+							)}
+						<br></br>
+						<br></br>
 					</div>
 				);
 			})}
