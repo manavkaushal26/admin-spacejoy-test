ae1ce9b323f6eec1b0f11c1c5774dbf8f944eeb7
Sagar Dixit <sagardixit92@github.com>
2020-12-15 20:58:58 +0530

    showed user response

--- _sections/Dashboard/userProjectMainPanel/ProjectDesignInteractionPanel/StyleQuizResponse/index.tsx
+++ _sections/Dashboard/userProjectMainPanel/ProjectDesignInteractionPanel/StyleQuizResponse/index.tsx
@@ -1,6 +1,16 @@
 import Image from "@components/Image";
 import fetcher from "@utils/fetcher";
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Heading = styled.h1`
+	font-size: 24px;
+	text-transform: capitalize;
+`;
+
+const StyledImage = styled(Image)`
+	max-width: 300px;
+`;
 
 interface LogObject {
 	isActive: boolean;
@@ -8,12 +18,22 @@ interface LogObject {
 	id: number;
 	description: string;
 }
+
+interface ActiveStyleObject {
+	cdn?: string;
+	name: string;
+}
+
 interface LogShape {
 	[key: string]: Array<LogObject>;
 }
 
 const UserStyleQuizResponse: React.FC<{ userId: string }> = ({ userId }) => {
 	const [userStyleLogs, setUserStyleLogs] = useState([]);
+	const [activeStyle, setActiveStyle] = useState<ActiveStyleObject>({
+		name: "",
+		cdn: "",
+	});
 
 	const fetchUserStyleResponse = async (userId: string): Promise<string> => {
 		const endPoint = `/v1/quizUsers/${userId}/results`;
@@ -25,6 +45,7 @@ const UserStyleQuizResponse: React.FC<{ userId: string }> = ({ userId }) => {
 				statusCode,
 			} = res;
 			if (statusCode <= 300) {
+				setActiveStyle(data[0]?.styleIds[0]);
 				return data[0].quizId || "";
 			} else {
 				throw new Error("");
@@ -63,19 +84,27 @@ const UserStyleQuizResponse: React.FC<{ userId: string }> = ({ userId }) => {
 	}, [userId]);
 	return (
 		<div>
+			<div>
+				{activeStyle?.name && <Heading>Style - {activeStyle?.name || ""}</Heading>}
+				{activeStyle?.cdn && (
+					<span>
+						<StyledImage src={`q_70,w_300,h_180/${activeStyle?.cdn}`} width='100%' />
+					</span>
+				)}
+			</div>
+			<br></br>
 			{userStyleLogs.map(userStyle => {
 				const styleCategory = Object.keys(userStyle)[0];
-				const values = Object.values(userStyle);
+				const values: { cdn: string }[][] = Object.values(userStyle);
 				return (
 					<div key={userStyle?.id}>
 						<h2 style={{ textTransform: "capitalize" }}>{styleCategory}</h2>
 						{values[0] &&
 							values[0].map(
 								(value: LogObject): JSX.Element => {
-									console.log("value", value);
 									return (
 										<>
-											<Image style={{ maxWidth: 300 }} src={`q_70,w_300,h_180/${value?.cdn}`} width='100%' />
+											<StyledImage src={`q_70,w_300,h_180/${value?.cdn}`} width='100%' />
 											&nbsp;
 										</>
 									);
